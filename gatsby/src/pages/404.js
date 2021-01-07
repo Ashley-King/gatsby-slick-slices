e36730fcb3a17a866cb8fec764a0e006d95a2e9e
Ashley <ashley@gmail.com>
2021-01-06 19:18:31 -0500

    initial pages, styles and sanity documents

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FourOhFourPage() {
+  return (
+    <>
+      <p>nope, that's not here.</p>
+    </>
+  );
+}
---
+++ gatsby/src/pages/beers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function BeersPage() {
+  return (
+    <>
+    
+      <p>Hey beer</p>
+    </>
+  );
+}
